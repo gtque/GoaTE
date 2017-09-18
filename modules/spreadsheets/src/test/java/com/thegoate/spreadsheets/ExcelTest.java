5c89bd61319187d54b130a4e9f97022a0ca16484
Eric Angeli <eric.angeli@onedatascan.com>
2017-09-18 11:06:05 -0400

    update license info, add spread sheet data loader

--- modules/spreadsheets/src/test/java/com/thegoate/spreadsheets/ExcelTest.java
+++ modules/spreadsheets/src/test/java/com/thegoate/spreadsheets/ExcelTest.java
@@ -1,10 +1,40 @@
+/*
+ * Copyright (c) 2017. Eric Angeli
+ *
+ *  Permission is hereby granted, free of charge,
+ *  to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy,
+ *  modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit
+ *  persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission
+ *  notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ *  AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ */
 package com.thegoate.spreadsheets;
 
+import com.thegoate.Goate;
+import com.thegoate.data.DataLoader;
+import com.thegoate.spreadsheets.data.SpreadSheetDL;
 import com.thegoate.spreadsheets.utils.SheetUtils;
 import com.thegoate.testng.TestNGEngineMethodDL;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
@@ -36,4 +66,23 @@ public class ExcelTest extends TestNGEngineMethodDL {
         assertEquals(sheet.get("c",0,"e"),"e");
         assertEquals(sheet.get(2,0,3),3);
     }
+
+    @Test(groups = {"unit"})
+    public void ExcelSheetDLTest() throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        DataLoader dl = new SpreadSheetDL().fileName("sample.xlsx").sheetName("Sheet1");
+        List<Goate> dataList = dl.load();
+        assertEquals(dataList.get(0).get("a"),"b");
+        assertEquals(dataList.get(1).get("z"),"42.01");
+        assertEquals(dataList.size(),2);
+    }
+
+    @Test(groups = {"unit"})
+    public void ExcelSheetDLTestNoHeader() throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        DataLoader dl = new SpreadSheetDL().fileName("sample.xlsx").sheetName("Sheet1").firstRowIsHeader(false);
+        List<Goate> dataList = dl.load();
+        assertEquals(dataList.get(0).get("0"),"a");
+        assertEquals(dataList.get(1).get("1"),"42");
+        assertEquals(dataList.size(),3);
+    }
+
 }
