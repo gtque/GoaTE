fe23f21c07aedaca45dcc05ddb9f009512007238
gtque <pilotg2.eric@gmail.com>
2019-08-06 18:46:57 -0400

    selenium:update version

--- modules/selenium/src/main/java/com/goate/selenium/staff/GoateDriver.java
+++ modules/selenium/src/main/java/com/goate/selenium/staff/GoateDriver.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2017. Eric Angeli
+ *
+ *  Permission is hereby granted, free of charge,
+ *  to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy,
+ *  modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit
+ *  persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission
+ *  notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ *  AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ */
 package com.goate.selenium.staff;
 
 import com.goate.selenium.annotations.Driver;
@@ -5,8 +31,8 @@ import com.thegoate.Goate;
 import com.thegoate.logging.BleatBox;
 import com.thegoate.logging.BleatFactory;
 import com.thegoate.utils.GoateUtils;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Base class for defining a driver builder object.
@@ -38,7 +64,7 @@ public abstract class GoateDriver {
         }
     }
     OS os;
-    DesiredCapabilities dc;
+    MutableCapabilities dc;
     boolean driverInstalled = false;
     String driverPath = "";
 
@@ -56,9 +82,9 @@ public abstract class GoateDriver {
      * Override this method to return the capabilities for the implemented web driver.
      * @return The desired capabilities for the given driver.
      */
-    protected abstract DesiredCapabilities loadCapabilities();
+    protected abstract MutableCapabilities loadCapabilities();
 
-    public DesiredCapabilities getDc(){
+    public MutableCapabilities getDc(){
         return dc;
     }
 
--- modules/selenium/src/main/java/com/goate/selenium/staff/OperaWebDriver.java
+++ modules/selenium/src/main/java/com/goate/selenium/staff/OperaWebDriver.java
@@ -1,8 +1,36 @@
+/*
+ * Copyright (c) 2017. Eric Angeli
+ *
+ *  Permission is hereby granted, free of charge,
+ *  to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy,
+ *  modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit
+ *  persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission
+ *  notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ *  AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ */
 package com.goate.selenium.staff;
 
 import com.goate.selenium.annotations.Driver;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
@@ -13,12 +41,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class OperaWebDriver extends GoateDriver {
 
     @Override
-    protected DesiredCapabilities loadCapabilities() {
-        return DesiredCapabilities.operaBlink();
+    protected MutableCapabilities loadCapabilities() {
+        return new OperaOptions();
     }
 
     @Override
     public WebDriver build() {
-        return new OperaDriver(dc);
+        return new OperaDriver((OperaOptions)dc);
     }
 }
